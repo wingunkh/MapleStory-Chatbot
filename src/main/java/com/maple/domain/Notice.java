76bbe1fd41ad24f6ab48e9e6f0941e4413af21b0
wingunkh <99gusrms@naver.com>
2024-07-22 22:07:13 +0900

    feat: 공지사항 공지 날짜에 요일 추가

--- src/main/java/com/maple/domain/Notice.java
+++ src/main/java/com/maple/domain/Notice.java
@@ -8,6 +8,8 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 @Entity
 @Data
@@ -20,18 +22,25 @@ public class Notice {
 
     private String url;
 
-    private LocalDate date;
+    private String date;
 
-    // OffsetDateTime → LocalDate 변환
-    public static LocalDate convertTime(String string) {
+    // OffsetDateTime → LocalDate + (요일) 변환
+    public static String convertTime(String string) {
         string = string.substring(1, string.length() - 1);
         // String.valueOf() 메서드로 인한 작은 따옴표 제거
 
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(string, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         // 문자열 → OffsetDateTime 파싱
 
-        return offsetDateTime.toLocalDate();
+        LocalDate localDate = offsetDateTime.toLocalDate();
         // OffsetDateTime → LocalDate 변환
         // ex) 2024-07-18T17:30+09:00 → 2024-07-18
+
+        String shortDayOfWeek = localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+        // 요일 추출
+        // ex) 목
+
+        return localDate + " (" + shortDayOfWeek + ")";
+        // ex) 2024-07-18 (목)
     }
 }
