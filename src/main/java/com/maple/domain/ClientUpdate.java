a2824863c8f8f2ae81ccb29a11db8e166eadda44
wingunkh <99gusrms@naver.com>
2024-07-25 21:37:33 +0900

    feat: ClientUpdate (업데이트) 엔티티 추가
    Update 예약어 이슈로 인해 ClientUpdate로 명명

---
+++ src/main/java/com/maple/domain/ClientUpdate.java
@@ -0,0 +1,45 @@
+package com.maple.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+@Entity
+@Data
+public class ClientUpdate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String url;
+
+    private String date;
+
+    public static String convertTime(String string) {
+        string = string.substring(1, string.length() - 1);
+        // String.valueOf() 메서드로 인한 작은 따옴표 제거
+
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(string, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        // 문자열 → OffsetDateTime 파싱
+
+        LocalDate localDate = offsetDateTime.toLocalDate();
+        // OffsetDateTime → LocalDate 변환
+        // ex) 2024-07-18T17:30+09:00 → 2024-07-18
+
+        String shortDayOfWeek = localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+        // 요일 추출
+        // ex) 목
+
+        return localDate + " (" + shortDayOfWeek + ")";
+        // ex) 2024-07-18 (목)
+    }
+}
