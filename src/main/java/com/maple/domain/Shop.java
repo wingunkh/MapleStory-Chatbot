3ce911585b70b53781ab6a7b9ca5e50cbbaffc6c
wingunkh <99gusrms@naver.com>
2024-07-30 22:10:27 +0900

    feat: Shop (캐시샵 공지사항) 엔티티 추가

---
+++ src/main/java/com/maple/domain/Shop.java
@@ -0,0 +1,25 @@
+package com.maple.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Shop extends Information {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String url;
+
+    private String startDate;
+
+    private String endDate;
+}
