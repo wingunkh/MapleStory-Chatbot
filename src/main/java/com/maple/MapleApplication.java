466e7b58460454f2a93b9abf64a691bcbda5233b
wingunkh <99gusrms@naver.com>
2024-08-04 16:26:21 +0900

    feat: 실제 서비스를 위한 설정
    @PostConstruct를 통해 앱 최초 실행 시 데이터 갱신, 이후 새벽 3시마다 데이터 갱신

--- src/main/java/com/maple/MapleApplication.java
+++ src/main/java/com/maple/MapleApplication.java
@@ -1,10 +1,5 @@
 package com.maple;
 
-import com.maple.service.EventService;
-import com.maple.service.NoticeService;
-import com.maple.service.ShopService;
-import com.maple.service.UpdateService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,21 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class MapleApplication {
-	private final NoticeService noticeService;
-	private final UpdateService updateService;
-	private final EventService eventService;
-	private final ShopService shopService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(MapleApplication.class, args);
-	}
-
-	// 스프링 빈의 이벤트 라이프사이클
-	// 스프링 컨테이너 생성 → 스프링 빈 생성 → 의존관계 주입 → 초기화 콜백 → 사용 → 소멸 전 콜백 → 스프링 종료
-	// @PostConstruct : 초기화 콜백 (빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출)
-	// @PreDestroy : 소멸 전 콜백 (빈이 소멸되기 직전에 호출)
-	@PostConstruct
-	public void init() {
-
 	}
 }
--- src/main/java/com/maple/batch/JobScheduler.java
+++ src/main/java/com/maple/batch/JobScheduler.java
@@ -1,5 +1,6 @@
 package com.maple.batch;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
@@ -18,6 +19,15 @@ public class JobScheduler {
 
     @Scheduled(cron = "0 0 3 * * ?") // 매일 새벽 3시
     public void runJob() {
+        executeJob();
+    }
+
+    @PostConstruct
+    // 스프링 빈의 이벤트 라이프사이클
+    // 스프링 컨테이너 생성 → 스프링 빈 생성 → 의존관계 주입 → 초기화 콜백 → 사용 → 소멸 전 콜백 → 스프링 종료
+    // @PostConstruct : 초기화 콜백 (빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출)
+    // @PreDestroy : 소멸 전 콜백 (빈이 소멸되기 직전에 호출)
+    public void init() {
         executeJob();
     }
 
